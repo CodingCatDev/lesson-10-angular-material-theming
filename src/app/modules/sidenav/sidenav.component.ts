455569e2d431d7ade235360b16ba0e3430ad7e78
Alex Patterson <admin@ajonp.com>
2019-01-17 07:32:48 -0500

    smaller title for mobile

--- src/app/modules/sidenav/sidenav.component.ts
+++ src/app/modules/sidenav/sidenav.component.ts
@@ -12,7 +12,7 @@ import { ColorPickerService } from 'src/app/core/services/color-picker.service';
 export class SidenavComponent implements OnInit, OnDestroy {
   @ViewChild('snav') public sidenav: MatSidenav;
   routerSub: Subscription;
-  title = 'Lesson 10 - Angular Material Theming';
+  title = 'Angular Material Theming';
   constructor(
     private router: Router,
     private colorPicker: ColorPickerService
